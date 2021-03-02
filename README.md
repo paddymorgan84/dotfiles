7733746295213edbc796f2177829ef865c8f9b69
paddymorgan84 <patrickmorgan1984@gmail.com>
2021-03-02 23:21:08 +0000

    Convert setup to script

--- README.md
+++ README.md
@@ -7,5 +7,4 @@ Setting up my environment quickly and consistently
 ```bash
 git clone git@github.com:paddymorgan84/dotfiles.git
 sh setup.sh
-make provision
 ```
--- setup.sh
+++ setup.sh
@@ -1,29 +1,65 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cp $PWD/git/.gitconfig.local $HOME/.gitconfig.local
-echo "Enter your full name";
-read name; \
-sed -i "s/GITNAME/$$name/" $HOME/.gitconfig
-echo "Enter your email address";
-read email; \
-sed -i "s/GITEMAIL/$$email/g" $HOME/.gitconfig
-echo "Enter your PAT";
-read pat; \
-sed -i "s/MYTOKEN/$$pat/g" $HOME/.oh-my-zsh/custom/exports.zsh
-echo "Enter your GitHub Org";
-read org; \
-sed -i "s/MYORG/$$org/g" $HOME/.oh-my-zsh/custom/exports.zsh
+set -e
+set -f
 
-echo "\e[92mUpdating apt...\e[0m"
-sudo apt update
-echo "\e[92mUpgrading apt...\e[0m"
-sudo apt upgrade
-echo "\e[92mInstalling Make...\e[0m"
-sudo apt install make
-echo "\e[92mInstalling Ansible...\e[0m"
-sudo apt install ansible
-echo "\e[92mInstalling Zsh...\e[0m"
-sudo apt install zsh
-echo "\e[92mInstalling Gh cli...\e[0m"
-wget https://github.com/cli/cli/releases/download/v0.6.4/gh_0.6.4_linux_amd64.deb
-sudo dpkg -i gh_*_linux_amd64.deb                                                                                                                                1 ↵
+printf "Set up my machine:\n"
+IGNORE_OMZ=${IGNORE_OMZ:-false}
+IGNORE_DOTFILES=${IGNORE_DOTFILES:-false}
+IGNORE_GIT=${IGNORE_GIT:-false}
+printf " - IGNORE_OMZ      = %s\n" "${IGNORE_OMZ}"
+printf " - IGNORE_DOTFILES = %s\n" "${IGNORE_DOTFILES}"
+printf " - IGNORE_GIT      = %s\n" "${IGNORE_GIT}"
+
+
+###
+# Install oh my zsh
+###
+if ! ${IGNORE_OMZ} ; then
+printf "\n🚀 Installing oh-my-zsh\n"
+if [ -d "${HOME}/.oh-my-zsh" ]; then
+  printf "oh-my-zsh is already installed\n"
+else
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  echo "Enter your GitHub PAT";
+	read -re pat
+	sed -i "s/MYTOKEN/${pat}/g" "${HOME}"/.oh-my-zsh/custom/exports.zsh
+
+	echo "Enter your GitHub Org";
+	read -re org
+	sed -i "s/MYORG/${org}/g" "${HOME}"/.oh-my-zsh/custom/exports.zsh
+fi
+fi
+
+
+###
+# Installing dotfiles
+###
+if ! ${IGNORE_DOTFILES} ; then
+printf "\n🚀 Installing dotfiles\n"
+ln -sf "$(PWD)"/zsh/.zshrc "${HOME}"/.zshrc
+ln -sf "$(PWD)"/zsh/paddy.zsh-theme "${HOME}"/.oh-my-zsh/custom/themes/
+ln -sf "$(PWD)"/zsh/aliases.zsh "${HOME}"/.oh-my-zsh/custom/aliases.zsh
+ln -sf "$(PWD)"/zsh/exports.zsh "${HOME}"/.oh-my-zsh/custom/exports.zsh
+ln -sf "$(PWD)"/zsh/functions.zsh "${HOME}"/.oh-my-zsh/custom/functions.zsh
+fi
+
+
+###
+# Installing git configuration
+###
+if ! ${IGNORE_GIT} ; then
+printf "\n🚀 Installing git configuration\n"
+if [ ! -f "${HOME}/.gitconfig.local" ] ; then
+  cp git/.gitconfig.local "${HOME}/.gitconfig.local"
+
+  echo "Enter your full name";
+  read -re var
+  sed -i '' "s/GITNAME/${var}/" "${HOME}/.gitconfig.local"
+
+  echo "Enter your email address";
+  read -re var
+  sed -i '' "s/GITEMAIL/${var}/" "${HOME}/.gitconfig.local"
+fi
+fi
