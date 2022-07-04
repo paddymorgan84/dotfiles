5e30afb8d172c6686218e87e034d15d17fffc6fa
paddymorgan84 <patrickmorgan1984@gmail.com>
2022-07-04 10:40:38 +0100

    Add linux brew

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -6,3 +6,6 @@ export GOPATH=$HOME/go
 export GOPRIVATE='github.com/paddymorgan84'
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+
+# Brew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
