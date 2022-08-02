f2cd137310f45c0cb3ef405e023bad378a77a309
paddymorgan84 <patrickmorgan1984@gmail.com>
2022-08-02 16:15:26 +0100

    Correct some Go issues after linuxbrew changes

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,6 +3,7 @@ export ANSIBLE_COW_SELECTION=random
 
 # Go
 export GOPATH=$HOME/go
+export GOROOT=$(brew --prefix golang)/libexec
 export GOPRIVATE='github.com/paddymorgan84'
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
